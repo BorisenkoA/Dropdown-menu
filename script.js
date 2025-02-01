d42cddac44e02c5f0a9971d2cf9b9d54aee9c238
Artem <artemborisencko@gmail.com>
2025-02-01 21:35:02 +0200

    Commit

--- script.js
+++ script.js
@@ -3,19 +3,37 @@ const showItem = document.querySelector(".select-wrap");
 const listItem = document.querySelectorAll(".select-wrap li");
 
 chooseColor.addEventListener("click", function (event) {
-  showItem.classList.toggle('hidden');
-  const isExpanded = chooseColor.getAttribute('aria-expanded') === 'true';
-  chooseColor.setAttribute('aria-expanded', !isExpanded);
+  showItem.classList.toggle("hidden");
+  const isExpanded = chooseColor.getAttribute("aria-expanded") === "true";
+  chooseColor.setAttribute("aria-expanded", !isExpanded);
   event.stopPropagation();
 });
 
 listItem.forEach((item) => {
-  item.addEventListener('click', function() {
-    showItem.classList.add('hidden');
+  item.addEventListener("click", function () {
+    showItem.classList.add("hidden");
     chooseColor.textContent = this.textContent;
-  })
+  });
 });
 
-document.addEventListener('click', function() {
-  showItem.classList.add('hidden');
-})
+document.addEventListener("click", function () {
+  showItem.classList.add("hidden");
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Escape" && !showItem.classList.contains("hidden")) {
+    showItem.classList.add("hidden");
+  }
+});
+
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter" || event.key === "Tab") {
+    event.preventDefault();
+
+    if (showItem.classList.contains("hidden")) {
+      showItem.classList.remove("hidden");
+    } else {
+      showItem.classList.add("hidden");
+    }
+  }
+});
