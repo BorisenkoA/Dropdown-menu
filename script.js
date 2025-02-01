cd8f94017e89c67d4a9fddd0e6815810aabe150f
Artem <artemborisencko@gmail.com>
2025-02-01 15:56:56 +0200

    Commit

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-const chooseColor = document.querySelector(".choose-color");
+const chooseColor = document.getElementById("menuButton");
 const showItem = document.querySelector(".select-wrap");
 const listItem = document.querySelectorAll(".select-wrap li");
 
 chooseColor.addEventListener("click", function (event) {
   showItem.classList.toggle('hidden');
+  const isExpanded = chooseColor.getAttribute('aria-expanded') === 'true';
+  chooseColor.setAttribute('aria-expanded', !isExpanded);
   event.stopPropagation();
 });
 
